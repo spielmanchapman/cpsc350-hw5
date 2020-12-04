d318922231972c94b0bbef60ce75ae3608304e33
CCow <44686625+CCow@users.noreply.github.com>
2020-12-03 22:48:45 -0800

    Add files via upload

--- faculty.cpp
+++ faculty.cpp
@@ -1,1 +1,35 @@
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class faculty{
+  faculty();
+  faculty(int idNum, string facName, string pos, string dept);
+  ~faculty();
+
+public:
+  int facultyID;
+
+  string facultyName;
+  string position;
+  string department;
+}
+
+faculty::faculty(){
+  facultyId = 0000000;
+  facultyName = "None";
+  position = "New Hire";
+  department = "Unknown";
+}
+
+faculty::faculty(int idNum, string facName, string pos, string dept){
+  this->facultyId = idNum;
+  this->facultyName = facName;
+  this->position = pos;
+  this->department = dept;
+}
+
+faculty::~faculty(){
+  //delete nothing
+}
