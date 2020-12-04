858010a89243f15db41f2dce3e8fc0e92e069556
CCow <44686625+CCow@users.noreply.github.com>
2020-12-03 22:36:57 -0800

    Add files via upload

--- student.cpp
+++ student.cpp
@@ -1,1 +1,42 @@
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class student{
+  student();
+  student(int idNum, string stuName, string grade, string major, double gpa, int adIdNum);
+  ~student();
+public:
+  int studentID;
+  int advisorID;
+
+  double Gpa;
+
+  string studentName;
+  string standing;
+  string major;
+};
+
+student::student(){
+  studentID = 0000000;
+  studentName = "None";
+  standing = "Unknown";
+  major = "Undeclared";
+  Gpa = 0.0;
+  advisorID = 0000000;
+
+}
+
+student::student(int idNum, string stuName, string grade, string major, double gpa, int adIdNum){
+  this->studentID = idNum;
+  this->studentName = stuName;
+  this->standing = grade;
+  this->major = major;
+  this->Gpa = gpa;
+  this->advisorID = adIdNum;
+}
+
+student::~student(){
+  //do nothing
+}
